6cdf39ab81c7c656edbc7126d0b29714b64e633a
Melody Linch <rita.gnatenko@gmail.com>
2022-09-24 22:56:09 +0700

    f

--- clock.py
+++ clock.py
@@ -1,16 +1,13 @@
+def seconds_to_time(sec):
+    hours = (sec // 3600) % 24
+    sec %= 3600
+    minutes = (sec // 60) % 60
+    sec = sec % 60
+    
+    return f'{hours}h {minutes}m {sec}s'
+
 time_before = int(input())
 time_after = time_before + int(input())
 
-hours = (time_before // 3600) % 24
-time_before %= 3600
-minutes = (time_before // 60) % 60
-seconds = time_before % 60
-
-print(hours, minutes, seconds)
-
-hours = (time_after // 3600) % 24
-time_after %= 3600
-minutes = (time_after // 60) % 60
-seconds = time_after % 60
-
-print(hours, minutes, seconds)
+print('Before: '+ seconds_to_time(time_before))
+print('After: '+ seconds_to_time(time_after))
